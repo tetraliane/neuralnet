5b6178a42178200720be3b2bd20bb594dd071ae8
Tetraliane <57061591+tetraliane@users.noreply.github.com>
2023-08-28 15:40:25 +0900

    Added tests for Add

--- src/layers/add.rs
+++ src/layers/add.rs
@@ -39,3 +39,69 @@ where
             .update(&mut self.bias, grad_out.sum_axis(Axis(0)))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use ndarray::{Array1, Array2, Ix1};
+
+    use crate::traits::{Layer, Optimizer};
+
+    use super::Add;
+
+    fn bias() -> Array1<usize> {
+        Array1::from_vec(vec![1, 2, 3])
+    }
+    fn input() -> Array2<usize> {
+        Array2::from_shape_vec((2, 3), vec![4, 5, 6, 7, 8, 9]).unwrap()
+    }
+    fn grad() -> Array2<usize> {
+        Array2::from_shape_vec((2, 3), vec![10, 11, 12, 13, 14, 15]).unwrap()
+    }
+    fn bias_after_update() -> Array1<usize> {
+        Array1::from_vec(vec![16, 17, 18])
+    }
+
+    struct DummyOpt;
+    impl Optimizer<usize, Ix1> for DummyOpt {
+        fn update(
+            &mut self,
+            param: &mut ndarray::Array<usize, Ix1>,
+            _: ndarray::Array<usize, Ix1>,
+        ) {
+            *param = bias_after_update()
+        }
+    }
+
+    #[test]
+    fn should_add_bias_to_input() {
+        let expected =
+            Array2::from_shape_vec((2, 3), vec![4 + 1, 5 + 2, 6 + 3, 7 + 1, 8 + 2, 9 + 3]).unwrap();
+
+        let layer = Add::new(bias(), DummyOpt);
+        let result = layer.forward(&input());
+
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn should_return_the_given_gradient() {
+        let layer = Add::new(bias(), DummyOpt);
+        let result = layer.backward(&grad(), &input());
+
+        assert_eq!(result, grad())
+    }
+
+    #[test]
+    fn should_call_optimizer_update() {
+        let mut layer = Add::new(bias(), DummyOpt);
+        layer.learn(&grad(), &input());
+
+        assert_eq!(layer.bias, bias_after_update())
+    }
+
+    #[test]
+    fn should_initialize_bias_with_zero() {
+        let layer = Add::<usize, _>::new_zero(3, DummyOpt);
+        assert_eq!(layer.bias, Array1::zeros(3))
+    }
+}
